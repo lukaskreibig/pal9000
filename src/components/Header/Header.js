19307b6b64ee8f93c7ee09a1001a9079fdb6d0a6
Anna Gosme <anna.gosme@gmail.com>
2021-05-11 16:14:13 +0200

    create grid wrapper for all pages and cleanup of project file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Header() {
     return (
         <div className="wrapper">
-            
+            <a href="/"><div className="header-logo"></div></a> 
+            <p className="header-text">Rock Shop</p>
+            <p className="header-text">Photo Journal</p>
             
         </div>
     )
