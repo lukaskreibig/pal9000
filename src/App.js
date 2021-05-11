a6456cbebcaa37b05b85af7b045992077e62a131
Anna Gosme <anna.gosme@gmail.com>
2021-05-11 17:47:00 +0200

    Header styling finished need to add links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import "./index.css";
 
 export default function App() {
   return (
     <div className="wrapper">
+      <Header />
       <Footer />
       
     </div>
