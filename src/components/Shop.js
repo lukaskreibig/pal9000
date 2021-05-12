70aae8987422e409061f9cf15160ee9d2127a58b
lukaskreibig <lukatze@posteo.de>
2021-05-12 10:13:48 +0200

    fixed header, new shop css

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -6,7 +6,9 @@ function Shop({ img, name, handleBuy, id }) {
     <div className="shop">
       {name}
       <img src={img} alt={name} width="150px" height="150px" />
-      <button onClick={() => handleBuy(id)}> I'm buying it </button>
+      <button className="buttonbuy" onClick={() => handleBuy(id)}>
+        Buy
+      </button>
     </div>
   );
 }
